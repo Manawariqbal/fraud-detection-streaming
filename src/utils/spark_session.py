f55da74f9c1dd2d5df29a24641d9ae31d62e72ad
manawariqbal <mmiqbalmanawar@gmail.com>
2025-12-30 23:55:51 +0530

    Added the updated code

--- src/utils/spark_session.py
+++ src/utils/spark_session.py
@@ -1,26 +1,26 @@
 from pyspark.sql import SparkSession
-import os
-def get_spark_session(app_name: str="FraudDetectionApp")->SparkSession:
+
+
+def get_spark_session(app_name: str = "FraudDetectionApp") -> SparkSession:
     """
-    -Creates and returns a SparkSession configured for:
-    - Local execution
-    - Kafka integration
-    - Structured Streaming
+    Create and return a SparkSession configured for local execution
+    and Kafka Structured Streaming.
     """
-    spark=(
-        SparkSession.Builder
+
+    builder = SparkSession.builder
+
+    spark = (
+        builder
+        .master("local[*]")
         .appName(app_name)
-        .master("local[*]")# Use all available cores
-        # Kafka package (comes with Spark 3.x, safe to keep explicit)
         .config(
             "spark.jars.packages",
-            "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1"
+            "org.apache.spark:spark-sql-kafka-0-10_2.13:4.1.0"
         )
-        # Streaming & fault tolerance configs
-        .config("spark.sql.streaming.checkpointLocation", "/tmp/spark-checkpoints")
         .config("spark.sql.shuffle.partitions", "4")
         .getOrCreate()
+    )
 
-        )
     spark.sparkContext.setLogLevel("WARN")
     return spark
+
